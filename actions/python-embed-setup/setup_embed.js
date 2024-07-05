95b75198f39dcf0a52b458ade36ab65cb5f7ebad
DoubtfulJerboa <david.johnston@seamlessintelligence.com.au>
2024-07-05 15:09:53 +0800

    no github?

--- actions/python-embed-setup/setup_embed.js
+++ actions/python-embed-setup/setup_embed.js
@@ -1,5 +1,5 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+// const core = require('@actions/core');
+// const github = require('@actions/github');
 
 const unzip = require('unzip-stream');
 const fs = require("fs");
