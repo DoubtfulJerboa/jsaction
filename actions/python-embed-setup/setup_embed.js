ddb8bea9e6f9acb55ff07f0132528a761c777544
DoubtfulJerboa <david.johnston@seamlessintelligence.com.au>
2024-07-05 15:02:50 +0800

    v6?

--- actions/python-embed-setup/setup_embed.js
+++ actions/python-embed-setup/setup_embed.js
@@ -7,6 +7,15 @@ const axios = require('axios');
 
 const exec = require('child_process').exec;
 
+
+async function unzippo(zipSource, zipDest) {
+  fsx.createReadStream(zipSource)
+  .pipe( unzip.Extract({ path: zipDest }));
+
+
+  console.log("extracted???")
+}
+
 async function main() {
 // try {
     // const {PYTHON_VERSION} = process.env
@@ -16,25 +25,45 @@ async function main() {
         console.error("Invalid python version")
         process.exit(1)
     }
+    // const fs = require("fs")
     let res = await axios.get(`https://www.python.org/ftp/python/${PYTHON_VERSION}/python-${PYTHON_VERSION}-embed-amd64.zip`).then(function (response) {
       return response.data});
     
     fs.writeFileSync("./python-embed.zip", Buffer.from(res), (err)=>{ 
         if (err) throw err;}); console.log('python-embed.zip saved'); 
 
+await unzippo("./python-embed.zip", "D:/Dev/TestScripts/jsaction/python-embed")    
+
+
+// fs.createReadStream("./python-embed.zip")
+// .pipe( unzip.Parse())
+// .on("entry", function (entry) {
+//   entry.pipe(fs.createWriteStream("./python-embed"))
+// });
+
+    // let target = path.join(__dirname, "/python-embed")
+    // await extract("./python-embed.zip", {dir: target})
+
     const pth = PYTHON_VERSION.split(".").slice(0,2).join("")
 
-    fs.appendFile('./python-embed/python'+pth+'._pth', '\nLib\nLib/site-packages\nScripts\n../\n../../', function (err) {
+    fs.appendFile(`./python-embed/python${pth}._pth`, '\nLib\nLib/site-packages\nScripts\n../\n../../', function (err) {
         if (err) throw err;
         console.log('Saved!');
     });
 
-    const pipResult = github.request("https://bootstrap.pypa.io/get-pip.py")
-    fs.writeFile("get-pip.py", Buffer.from(pipResult.data), (err)=>{ if (err) throw err; console.log('File saved');})
+    res = await axios.get("https://bootstrap.pypa.io/get-pip.py").then(function (response) {return response.data});
+
+    fs.writeFileSync("get-pip.py", Buffer.from(res), (err)=>{ if (err) throw err;});
+    console.log('get-pip.py saved');
+    
 
     exec(".\\python-embed\\python.exe .\\get-pip.py")
 
 
+  // } catch (error) {
+  //   core.setFailed(error.message);
+    
+  // }
       }
 
 main()
