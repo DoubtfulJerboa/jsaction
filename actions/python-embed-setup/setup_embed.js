abdc70aab1119a34d92e58be699f687862bfd3d9
DoubtfulJerboa <david.johnston@seamlessintelligence.com.au>
2024-07-11 16:44:02 +0800

    logging

--- actions/python-embed-setup/setup_embed.js
+++ actions/python-embed-setup/setup_embed.js
@@ -25,28 +25,20 @@ try {
         console.error("Invalid python version")
         process.exit(1)
     }
-    // const fs = require("fs")
+
+    console.log("Fetching python-embed.zip")
     let res = await axios({method: "get", url:`https://www.python.org/ftp/python/${PYTHON_VERSION}/python-${PYTHON_VERSION}-embed-amd64.zip`, responseType: "arraybuffer"}).then(function (response) {
       return response.data});
     
     fs.writeFileSync("./python-embed.zip", Buffer.from(res), (err)=>{ 
         if (err) throw err;}); console.log('python-embed.zip saved'); 
 
+    console.log("Unzipping python-embed")
     await unzippo("./python-embed.zip", "./python-embed")    
-
-/*
-fs.createReadStream("./python-embed.zip")
-.pipe( unzip.Parse())
-.on("entry", function (entry) {
-  entry.pipe(fs.createWriteStream("./python-embed"))
-});
-let target = path.join(__dirname, "/python-embed")
-await extract("./python-embed.zip", {dir: target})
-
-*/
 
     const pth = PYTHON_VERSION.split(".").slice(0,2).join("")
 
+    console.log("Appending to python path")
     fs.appendFile(`./python-embed/python${pth}._pth`, '\nLib\nLib/site-packages\nScripts\n../\n../../', function (err) {
         if (err) throw err;
         console.log('Saved!');
@@ -54,13 +46,23 @@ await extract("./python-embed.zip", {dir: target})
 
     res = await axios.get("https://bootstrap.pypa.io/get-pip.py").then(function (response) {return response.data});
 
+    console.log("Fetching get-pip.py")
     fs.writeFileSync("get-pip.py", Buffer.from(res), (err)=>{ if (err) throw err;});
     console.log('get-pip.py saved');
     
+    console.log("Installing pip")
 
     execSync(".\\python-embed\\python.exe .\\get-pip.py")
 
+    console.log("Pip installed!")
+
+    console.log("Installing python requirements")
+
     execSync(`.\\python-embed\\python.exe -m pip install -r ${REQUIREMENTS_PATH}`)
+
+    console.log("Python requirements intalled")
+    
+
   } catch (error) {
     core.setFailed(error.message);
    
