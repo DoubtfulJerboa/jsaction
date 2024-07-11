9d7cd96adf38c466d52a5d4f69556d89b5e2da69
DoubtfulJerboa <david.johnston@seamlessintelligence.com.au>
2024-07-11 16:44:19 +0800

    recompile

--- actions/python-embed-setup/dist/index.js
+++ actions/python-embed-setup/dist/index.js
@@ -39366,7 +39366,7 @@ const StreamZip = __nccwpck_require__(2920);
 const fs = __nccwpck_require__(7147);
 const axios = __nccwpck_require__(4871);
 
-const exec = (__nccwpck_require__(2081).exec);
+const execSync = (__nccwpck_require__(2081).execSync);
 
 
 async function unzippo(zipSource, zipDest) {
@@ -39379,35 +39379,27 @@ async function unzippo(zipSource, zipDest) {
 
 async function main() {
 try {
-    // const {PYTHON_VERSION} = process.env
-    const PYTHON_VERSION = "3.11.4"
+    const PYTHON_VERSION = core.getInput("PYTHON_VERSION")
+    const REQUIREMENTS_PATH = core.getInput("REQUIREMENTS_PATH")
     const match = PYTHON_VERSION.match("^[0-9]\.[0-9]{1,2}\.[0-9]{1,2}$")
     if (!match){
         console.error("Invalid python version")
         process.exit(1)
     }
-    // const fs = require("fs")
+
+    console.log("Fetching python-embed.zip")
     let res = await axios({method: "get", url:`https://www.python.org/ftp/python/${PYTHON_VERSION}/python-${PYTHON_VERSION}-embed-amd64.zip`, responseType: "arraybuffer"}).then(function (response) {
       return response.data});
     
     fs.writeFileSync("./python-embed.zip", Buffer.from(res), (err)=>{ 
         if (err) throw err;}); console.log('python-embed.zip saved'); 
 
+    console.log("Unzipping python-embed")
     await unzippo("./python-embed.zip", "./python-embed")    
-
-/*
-fs.createReadStream("./python-embed.zip")
-.pipe( unzip.Parse())
-.on("entry", function (entry) {
-  entry.pipe(fs.createWriteStream("./python-embed"))
-});
-let target = path.join(__dirname, "/python-embed")
-await extract("./python-embed.zip", {dir: target})
-
-*/
 
     const pth = PYTHON_VERSION.split(".").slice(0,2).join("")
 
+    console.log("Appending to python path")
     fs.appendFile(`./python-embed/python${pth}._pth`, '\nLib\nLib/site-packages\nScripts\n../\n../../', function (err) {
         if (err) throw err;
         console.log('Saved!');
@@ -39415,12 +39407,22 @@ await extract("./python-embed.zip", {dir: target})
 
     res = await axios.get("https://bootstrap.pypa.io/get-pip.py").then(function (response) {return response.data});
 
+    console.log("Fetching get-pip.py")
     fs.writeFileSync("get-pip.py", Buffer.from(res), (err)=>{ if (err) throw err;});
     console.log('get-pip.py saved');
     
+    console.log("Installing pip")
 
-    exec(".\\python-embed\\python.exe .\\get-pip.py")
+    execSync(".\\python-embed\\python.exe .\\get-pip.py")
 
+    console.log("Pip installed!")
+
+    console.log("Installing python requirements")
+
+    execSync(`.\\python-embed\\python.exe -m pip install -r ${REQUIREMENTS_PATH}`)
+
+    console.log("Python requirements intalled")
+    
 
   } catch (error) {
     core.setFailed(error.message);
